3f8cfaf54469964db3991b1fd110d60ee3205ffe
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2025-02-02 02:35:36 +0200

    Useffect project exercise finished.

--- useEffect/01-starting-project/src/App.jsx
+++ useEffect/01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -8,13 +8,13 @@ import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
 const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-const storedPlaces = storedIds.map ((id) => 
-AVAILABLE_PLACES.find((place) => place.id === id));
+const storedPlaces = storedIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id));
 
 function App() {
 
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
@@ -32,13 +32,13 @@ function App() {
 
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
-   
+
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -47,12 +47,12 @@ function App() {
         return prevPickedPlaces;
       }
 
-      const place = AVAILABLE_PLACES.find((place) => place.id === id); 
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
 
     const storeIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-    if (storedIds.indexOf(id) === -1){
+    if (storedIds.indexOf(id) === -1) {
       localStorage.setItem(
         'selectedPlaces',
         JSON.stringify([id, ...storedIds])
@@ -60,23 +60,23 @@ function App() {
     }
 
   }
+const handleRemovePlace = useCallback(function handleRemovePlace() {
+  setPickedPlaces((prevPickedPlaces) =>
+    prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+  );
+  setModalIsOpen(false);
 
-  function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-    );
-    modal.current.close();
-
-    JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-    localStorage.setItem(
-      'selectedPlaces',
-      JSON.stringify(storedIds.filter ((id) => id !== selectedPlace.current))
-    );
-  }
+  const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  localStorage.setItem(
+    'selectedPlaces',
+    JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+  );
+}, []);
+  
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
