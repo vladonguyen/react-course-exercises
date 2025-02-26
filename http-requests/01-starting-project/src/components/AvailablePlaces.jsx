313d7b82087e79e40fa202b5b3db52c44aec6f41
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2025-02-27 00:10:07 +0200

    Using async await for fetch req

--- http-requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ http-requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  fetch('http://localhost:3000/places')
-  .then((response) => {
-    return response.json()
-  })
-  .then((resData)=> {
-    setAvailablePlaces(resData.places);
-  });
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+
+    }
+    fetchPlaces();
+  }, []
+  );
+
+
 
   return (
     <Places
